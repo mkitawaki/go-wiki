a08284c941d7597f091f94abc21d75c067a62dff
mkitawaki <watayakit@gmail.com>
2015-12-23 14:14:28 +0900

    add analogy

--- main.go
+++ main.go
@@ -6,10 +6,24 @@ import (
 	"net/http"
 	"os"
 	"strconv"
+	"strings"
 )
 
 type Distance struct {
 	SpecifiedWord string   `json:"specified_word"`
+	Words         []w.Pair `json:"words"`
+}
+
+type Analogy struct {
+	Is    string   `json:"is"`
+	To    string   `json:"to"`
+	What  string   `json:"what"`
+	Words []w.Pair `json:"words"`
+}
+
+type MostSimilarity struct {
+	PositiveWords []string `json:"positive_words"`
+	NegativeWords []string `json:"negative_words"`
 	Words         []w.Pair `json:"words"`
 }
 
@@ -35,6 +49,8 @@ func main() {
 	router := gin.Default()
 
 	router.GET("/distance/:word/:count", distance)
+	router.GET("/analogy/:is/:to/:what/:count", analogy)
+	router.GET("/mostSimilarity/:positives/:negatives/:count", mostSimilarity)
 	router.GET("/similarity/:x/:y", similarity)
 
 	router.Run(":" + os.Getenv("PORT"))
@@ -63,6 +79,72 @@ func distance(c *gin.Context) {
 	}
 	c.JSON(http.StatusOK, &Distance{
 		SpecifiedWord: word,
+		Words:         pairs,
+	})
+}
+
+func analogy(c *gin.Context) {
+	is := c.Param("is")
+	to := c.Param("to")
+	what := c.Param("what")
+	count := c.Param("count")
+	positives := []string{what, to}
+	negatives := []string{is}
+	cnt, err := strconv.Atoi(count)
+	if err != nil {
+		c.JSON(http.StatusOK, &Analogy{
+			Is:    is,
+			To:    to,
+			What:  what,
+			Words: []w.Pair{},
+		})
+		return
+	}
+	pairs, err := Word2Vec.MostSimilar(positives, negatives, cnt)
+	if err != nil {
+		c.JSON(http.StatusOK, &Analogy{
+			Is:    is,
+			To:    to,
+			What:  what,
+			Words: []w.Pair{},
+		})
+		return
+	}
+	c.JSON(http.StatusOK, &Analogy{
+		Is:    is,
+		To:    to,
+		What:  what,
+		Words: pairs,
+	})
+}
+
+func mostSimilarity(c *gin.Context) {
+	p := c.Param("positives")
+	n := c.Param("negatives")
+	count := c.Param("count")
+	positives := strings.Split(p, "+")
+	negatives := strings.Split(n, "+")
+	cnt, err := strconv.Atoi(count)
+	if err != nil {
+		c.JSON(http.StatusOK, &MostSimilarity{
+			PositiveWords: positives,
+			NegativeWords: negatives,
+			Words:         []w.Pair{},
+		})
+		return
+	}
+	pairs, err := Word2Vec.MostSimilar(positives, negatives, cnt)
+	if err != nil {
+		c.JSON(http.StatusOK, &MostSimilarity{
+			PositiveWords: positives,
+			NegativeWords: negatives,
+			Words:         []w.Pair{},
+		})
+		return
+	}
+	c.JSON(http.StatusOK, &MostSimilarity{
+		PositiveWords: positives,
+		NegativeWords: negatives,
 		Words:         pairs,
 	})
 }
